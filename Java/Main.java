5d2ae08ac7d2718b56e09a1a3202c4786310576a
jaydeepravat78 <jaydeepr.inexture@gmail.com>
2022-03-04 16:38:21 +0530

    added a java file

---
+++ Java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println(multiply(20,30);
+	}
+	public static int multiply(int a, int b){
+		return a * b;
+	}
+}
